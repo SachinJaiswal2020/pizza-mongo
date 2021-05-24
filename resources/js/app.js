398ac8adf38bc762b31d19bc960b94d95fdfa199
sachinjayswal <sachinjayswal143.rs@gmail.com>
2021-05-24 17:31:23 +0530

    Session & Add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-console.log('Hello from app js')
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-class')
+let cartCounter = document.querySelector('#cartCounter')
+function updateCart(pizza){
+    axios.post('/update-cart', pizza).then(res => {
+        // console.log(res)
+        cartCounter.innerText = res.data.totalQty
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item added to cart',
+            progressBar: false,
+            layout: 'topRight'
+        }).show();
+    }).catch(err =>{
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            text: 'Something went wrong',
+            progressBar: false,
+            layout: 'topRight'
+        }).show();
+    })
+}
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click',(e) =>{
+        // console.log(e)
+        let pizza = JSON.parse(btn.dataset.pizza)
+        updateCart(pizza)
+        // console.log(pizza)
+    })
+})
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' });
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 8000
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')
 
 // if(process.env.PORT){
 //     PORT = process.env.PORT;
@@ -14,18 +19,33 @@ const mongoose = require('mongoose');
 
 
 //MONGODB CONNECTION
-const DB = 'mongodb+srv://sachinjaiswal:sachin@cluster0.iy1to.mongodb.net/pizza?retryWrites=true&w=majority';
+require('./DB/conn')
 
-mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true 
-}).then(()=>{
-    console.log('Database connected...');
-}).catch(err => {
-    console.log('Connection failed...')
-});
+//SESSION CONFIG & SESSION STORE
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDbStore.create({
+        mongoUrl: process.env.DATABASE
+    }),
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 *24 }
+})
+)
 
+
+app.use(flash())
 
 //ASSEST
 app.use(express.static('public'))
+app.use(express.json())
+
+
+//GLOBAL MIDDLEWARE
+app.use((req,res,next) =>{
+    res.locals.session = req.session
+    next()
+})
 
 //Layout Template
 app.use(expressLayout)
